32e734bc7d94a9071d79f318fa0695d57cac81b7
antciccone <aciccone@bu.edu>
2017-02-13 12:00:53 -0700

    spec for if a user can create an account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,22 @@ class UsersController < ApplicationController
   def index
 
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to links_path
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name, :password, :password_confirmation)
+  end
 end
--- spec/features/users/a_visitor_can_create_a_user_spec.rb
+++ spec/features/users/a_visitor_can_create_a_user_spec.rb
@@ -14,10 +14,13 @@ describe 'As a visitor' do
 
   it "I can make an account" do
     visit new_user_path
-    fill_in :name, with: "Anthony"
-    fill_in :emai, with: 'aciccone2014@gmail.com'
-    fill_in :password, with: "123"
-    fill_in :password_confirmation, with: "123"
+    fill_in :Name, with: "Anthony"
+    fill_in :Email, with: 'aciccone2014@gmail.com'
+    fill_in :Password, with: "123"
+    fill_in :Password_confirmation, with: "123"
+    click_on "Create Account"
 
+    expect(current_path).to eq(links_path)
+    expect(page).to have_content("You have created an account")
   end
 end

df0544aab33b489b656ccc330e730330744c4fd1
antciccone <aciccone@bu.edu>
2017-02-13 17:30:18 -0700

    mark as read button on index page of links

--- app/assets/javascripts/mark_read.js.es6
+++ app/assets/javascripts/mark_read.js.es6
@@ -4,16 +4,7 @@ $( document ).ready(function(){
 
 function markAsRead(e) {
   e.preventDefault();
-
-  var $link = $(this).parents('.link');
-  var linkId = $link.data('link-id');
-
-  $.ajax({
-    type: "PATCH",
-    url: "/api/v1/links/" + linkId,
-    data: { read: true },
-  }).then(updateLinkStatus)
-    .fail(displayFailure);
+  axios.put('http://localhost:3000/api/v1/links/' + this.id)
 }
 
 function updateLinkStatus(link) {
--- spec/features/links/a_user_can_fill_out_a_links_form_spec.rb
+++ spec/features/links/a_user_can_fill_out_a_links_form_spec.rb
@@ -19,7 +19,25 @@ describe "As a Logged in User" do
       expect(page).to have_content("https://www.linkedin.com/")
       expect(page).to have_content("LinkedIn")
       expect(page).to have_content("false")
-
     end
+  end
+
+  it "i see an error if an enter an invalid url", :js => true do
+    User.create(email: 'test@test.com', password_digest: BCrypt::Password.create("test"))
+
+    visit login_path
+    fill_in :Email, with: 'test@test.com'
+    fill_in :Password, with: "test"
+    click_on "Log In!"
+
+
+    fill_in :Url, with: "//www.linkedin.com/"
+    fill_in :Title, with: "LinkedIn"
+    click_on "Submit Link"
+
+    expect(page).to have_content("https://www.linkedin.com/")
+    expect(page).to have_content("LinkedIn")
+    expect(page).to have_content("false")
+
   end
 end

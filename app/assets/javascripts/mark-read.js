91e7b782ef8cf0936a2656feefc7d7210e9318a8
antciccone <aciccone@bu.edu>
2017-02-27 13:05:40 -0700

    attempting to do search

--- app/assets/javascripts/mark-read.js
+++ app/assets/javascripts/mark-read.js
@@ -1,5 +1,23 @@
 var $newLinkTitle, $newLinkUrl;
 
+function hotreads(linkURL) {
+  axios.post('http://localhost:8080/api/v1/links', {
+    url: "hi",
+  })
+  .then(function(response){
+    console.log(response)
+  })
+  .catch(function(error){
+    console.log(error)
+  })
+}
+
+function markHot() {
+  $('.url-link')[0].parentElement.append("Hottest Read")
+  $('.url-link')[1].parentElement.append("Hot Link")
+
+}
+
 $(document).ready(function(){
   $("body").on("click", ".read", function(){
     this.parentElement.children[2].innerHTML = "read: true"
@@ -11,5 +29,25 @@ $(document).ready(function(){
       method: 'PATCH',
       data: {read: true}
     });
+
+    hotreads()
   })
+
+  markHot()
+  $("#myInput").on('keyup', function() {
+    debugger;
+    var filter = this.value.toUpperCase();
+    var search = $('.hot-read')
+    for (i = 0; i < search.length; i++) {
+      debugger;
+     td = search[i].getElementsByClassName('url-link')[0]
+     if (td) {
+       if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+         search[i].style.display = "";
+       } else {
+         search[i].style.display = "none";
+       }
+     }
+   }
+  });
 })
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,11 +1,12 @@
 class Link < ApplicationRecord
-  scope :hot, -> {
-    select('links.url as url')
-      .joins('join reads on reads.link_id = links.id')
-      .where('reads.created_at > ?', Time.now - 1.day)
-      .group("links.url")
-      .order('count("reads".id) DESC').limit(10)
-  }
+
+
+  def self.top_ten
+    # links = where(read: true)
+    links = where(updated_at: (Time.now - 24.hours)..Time.now)
+    grouped = links.group_by{|link| link.url}
+    grouped.values.map {|similiar_links| similiar_links.first}
+  end
 
   has_many :reads,  dependent: :destroy
   belongs_to :user

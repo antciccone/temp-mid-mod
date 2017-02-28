28cfcd65458490509252e39532855027b69ab7e3
antciccone <aciccone@bu.edu>
2017-02-27 18:43:07 -0700

    passing test for ordered api endpoint"

--- spec/api/v1/links_request_spec.rb
+++ spec/api/v1/links_request_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
-
 describe 'index' do
   context 'api/vi/links' do
     it 'returns top ten read links' do
-      link2 = Link.create(title: "yahoo", url: "https://www.yahoo.com/", read: true, created_at: "2017-02-27 22:25:36", updated_at: "2017-02-27 22:25:46", user_id: nil)
-      link3 = Link.create(title: "espn", url: "https://www.espn.com/", read: false, created_at: "2017-02-27 22:25:36", updated_at: "2017-02-27 22:25:46", user_id: nil)
+      link2 = Link.create(title: "yahoo", url: "https://www.yahoo.com/", read: true)
+      link3 = Link.create(title: "google", url: "https://www.google.com/", read: true)
+      link = Link.create(title: "espn", url: "https://www.espn.com/", read: false, created_at: "2017-02-27 22:25:36", updated_at: "2017-02-27 22:25:46", user_id: nil)
       Read.create(link_id: link2.id)
+      Read.create(link_id: link3.id)
 
       get '/api/v1/links'
 
@@ -15,8 +16,22 @@ describe 'index' do
       expect(response).to be_success
       expect(response.status).to eq(200)
 
-      expect(links.count).to eq(1)
-      expect(links.first["url"]).to eq('https://www.yahoo.com/')
+      expect(links.count).to eq(2)
+      expect(links.first["url"]).to eq('https://www.google.com/')
     end
+  end
+
+  it 'links are displayed in order of most read' do
+    link2 = Link.create(title: "yahoo", url: "https://www.yahoo.com/", read: true)
+    link3 = Link.create(title: "google", url: "https://www.google.com/", read: true)
+    Read.create(link_id: link2.id)
+    Read.create(link_id: link2.id)
+    Read.create(link_id: link3.id)
+
+    get '/api/v1/links'
+
+    links = JSON.parse(response.body)
+    expect(links.first["url"]).to eq('https://www.yahoo.com/')
+    expect(links.second["url"]).to eq('https://www.google.com/')
   end
 end
